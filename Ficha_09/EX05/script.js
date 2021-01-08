7fd72f821da28c0fbc972952f635e6f7a06db46f
JoaoffPinho <joao.f.pinho@gmail.com>
2021-01-08 16:59:46 +0000

    ex5f9

---
+++ Ficha_09/EX05/script.js
@@ -0,0 +1,11 @@
+
+
+function calcdogyear(){
+    let year = prompt("How old is your doggie ?!")
+    let dogyear = year * 7
+    console.log("Your doggie is "+ dogyear + " in dog years!")
+}
+
+calcdogyear()
+calcdogyear()
+calcdogyear()
