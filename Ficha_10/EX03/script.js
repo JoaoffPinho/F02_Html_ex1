eca95f968312ad7e6fdd838f3a196f7faaf761ee
JoaoffPinho <joao.f.pinho@gmail.com>
2021-01-15 16:29:32 +0000

    ex3f10

---
+++ Ficha_10/EX03/script.js
@@ -0,0 +1,10 @@
+let color_array = ["red","orange","yellow","pink"]
+let position_Array = ["st","nd","rd","th"]
+
+for(let i = 0; i < color_array.length;i++){
+    console.log("My #"+[i+1]+" color is "+color_array[i])
+}
+
+for(let i = 0; i < color_array.length;i++){
+    console.log("My "+[i+1]+position_Array[i]+" color is "+color_array[i])
+}
