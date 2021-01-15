30f7dda66c5c5caa65f9e5e4483a624a52ae3dbc
JoaoffPinho <joao.f.pinho@gmail.com>
2021-01-15 16:56:24 +0000

    ex1f11

---
+++ Ficha_11/EX01/script.js
@@ -0,0 +1,7 @@
+function  jsstyle() {
+    let textelement= document.getElementById("text")
+
+    textelement.style.fontSize = '14'
+    textelement.style.fontFamily = 'Comic Sans MS'
+    textelement.style.color='green'
+}
